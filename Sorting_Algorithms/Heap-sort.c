3b43437ccac9b4fff4ab1a78156e8ec397893791
Nivesh <59831140+Niveshpai@users.noreply.github.com>
2021-02-28 20:31:01 +0530

    Update Heap-sort.c

--- Sorting_Algorithms/Heap-sort.c
+++ Sorting_Algorithms/Heap-sort.c
@@ -11,17 +11,17 @@ void heapify(int arr[], int size, int i)
   int right = 2*i + 2;
 
   if (left < size && arr[left] >arr[largest])
-  largest = left;
+    largest = left;
 
   if (right < size && arr[right] > arr[largest])
-  largest = right;
+    largest = right;
 
   if (largest != i)
   {
-    temp = arr[i];
+      temp = arr[i];
       arr[i]= arr[largest];
       arr[largest] = temp;
-   heapify(arr, size, largest);
+      heapify(arr, size, largest);
   }
 }
 
@@ -39,22 +39,21 @@ void heapSort(int arr[], int size)
   }
 }
 
-
 void readarray(int arr[],int n)
 {
     int i;
     for(i=0;i<n;i++)
     {
-        scanf("%d",&arr[i]);
+      scanf("%d",&arr[i]);
     }
 }
 
 void displayarray(int arr[],int n)
 {
-     int i;
+    int i;
     for(i=0;i<n;i++)
     {
-        printf("%d\t",arr[i]);
+      printf("%d\t",arr[i]);
     }
 }
 
