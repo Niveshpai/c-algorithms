0bbef55cb83d776418d1b5973b15e4ed0f0ad457
Nivesh <59831140+Niveshpai@users.noreply.github.com>
2021-02-28 20:32:43 +0530

    Create Bubble-sort.c

---
+++ Sorting_Algorithms/Bubble-sort.c
@@ -0,0 +1,39 @@
+// C program to implement Bubble Sort in C
+
+#include<stdio.h>
+#include<stdlib.h>
+
+void bubblesort(int a[],int n)
+{
+  int i,j,temp;
+  for(i=0;i<=n-2;i++)
+  {
+    for(j=0;j<=n-2-i;j++)
+    {
+      if(a[j+1]<a[j])
+      {
+        temp=a[j];
+        a[j]=a[j+1];
+        a[j+1]=temp;
+      }
+    }
+  }
+  printf("elements after sorting\n");
+  for(i=0;i<n;i++)
+  {
+     printf("%d\t",a[i]);
+  }
+}
+
+int main()
+{
+  int i,n,a[10];
+  printf("enter the number of elements=");
+  scanf("%d",&n);
+  for(i=0;i<n;i++)
+  {
+   scanf("%d",&a[i]);
+  }
+  bubblesort(a,n);
+  return 0;
+}
